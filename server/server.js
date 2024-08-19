ce7c34d461e6a630e5b2e4da6fdf521f8bd08ba1
bradleyyang <bradley.yang023@gmail.com>
2024-08-18 21:41:11 -0400

    got some route endpoints working

--- server/server.js
+++ server/server.js
@@ -1,21 +1,12 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const restaurantRoutes = require('./routes/restaurantRoutes');
+const Restaurant = require('./models/restaurant');
 
-const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000
 
 const express = require('express')
 const app = express()
-
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.send({
-    "name": "Bradley",
-    "age": 18
-  })
-})
+app.use(express.json());
 
 
 
@@ -31,3 +22,21 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((error) => {
     console.log(error)
   })
+
+
+// Routes
+app.post('/restaurants', async (req, res) => {
+  console.log(req.body.name);
+  const restaurant = new Restaurant(req.body);
+  await restaurant.save();
+  res.send(restaurant);
+});
+
+app.get('/restaurants', async (req, res) => {
+  try {
+    const restaurants = await Restaurant.find({});
+    res.send(restaurants);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
