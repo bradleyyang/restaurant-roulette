c6028d0d20d25703e880b627946d713b0110ca5f
bradleyyang <bradley.yang023@gmail.com>
2024-10-07 22:20:50 -0400

    fixed some routes

---
+++ server/models/User.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    username: {
+        type: String,
+        required: true,
+        minlength: 1,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+
+    },
+    gender: {
+        type: String,
+        enum: ["Male", "Female", "Other"],
+    },
+    phoneNumber: {
+        type: String
+    }
+
+});
+
+userSchema.path('username').validate(function (value) {
+    return value.trim().length > 0;
+}, 'Username cannot be empty.');
+
+
+userSchema.path('firstName').validate(function (value) {
+    return value.trim().length > 0; 
+}, 'First name cannot be empty.');
+
+userSchema.path('lastName').validate(function (value) {
+    return value.trim().length > 0; 
+}, 'Last name cannot be empty.');
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const Restaurant = require('./models/restaurant');
 const axios = require('axios');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
@@ -28,83 +27,35 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 // Routes
-app.post('/restaurants', async (req, res) => {
-  const restaurant = new Restaurant(req.body);
-  await restaurant.save();
-  res.send(restaurant);
-});
-
-app.get('/restaurants', async (req, res) => {
-  try {
-    const restaurants = await Restaurant.find({});
-    res.send(restaurants);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-app.get('/restaurant', async (req, res) => {
-  try {
-    const query = { name: "Bob's Burgers" };
-    const restaurant = await Restaurant.findOne(query);
-
-    if (!restaurant) {
-      return res.status(404).send({ message: 'Restaurant not found' });
-    }
-
-    res.send(restaurant);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-app.delete('/restaurants', async (req, res) => {
-  const query = { name: "David" };
-  const deleteResult = await Restaurant.deleteOne(query);
-  res.send(deleteResult);
-});
-
-app.put('/restaurants', async (req, res) => {
-  const filter = { name: "Starbucks" };
-
-  // this option instructs the method to create a document if no documents match the filter
-  const options = { upsert: true };
-  // create a document that sets the distance
-  const updateDoc = {
-    $set: {
-      name: "McDonalds",
-      open_now: true,
-      formatted_address: "5555 Dundas St W",
-      rating: 4.0,
-      distance: 1.2,
-      price_level: 3,
-      user_ratings_total: 100
-    },
-  };
-
-  const updateResult = await Restaurant.updateOne(filter, updateDoc, options);
-  res.send(updateResult);
-});
-
 
 app.get('/restaurant-details', async (req, res) => {
-  // Replace with the actual place ID you want to use
-  const placeId = 'ChIJ5_4bHL43K4gRlMRkVLS4ONg'; // Example place ID
-  const fields = 'name,rating,formatted_address,opening_hours,reviews'; // Fields you want to retrieve
+  const placeName = req.query.place_name;
+  const apiKey = process.env.GOOGLE_API_KEY;
+  
 
-  const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=${fields}&key=${process.env.GOOGLE_API_KEY}`;
-
+  const findPlaceEndpoint = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${placeName}&inputtype=textquery&key=${apiKey}`;
+  
   try {
-    // Make the HTTP request to the Google Places API
-    const response = await axios.get(url);
+    const placeIDResponse = await axios.get(findPlaceEndpoint);
+    const placeID = placeIDResponse.data.candidates[0].place_id;
+    
 
-    // Send the API response data back to the client
-    res.json(response.data);
+    if (!placeID) {
+      return res.status(404).json({ message: 'Place not found' });
+    }
+
+
+    const fields = 'name,rating,formatted_address,opening_hours,reviews';
+    const placeDetailsEndpoint = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeID}&fields=${fields}&key=${apiKey}`;
+    
+    const placeDetailsResponse = await axios.get(placeDetailsEndpoint);
+    res.status(200).json(placeDetailsResponse.data);
   } catch (error) {
-    console.error('Error fetching restaurant details:', error.message);
-    res.status(500).json({ error: 'Failed to fetch restaurant details' });
+    console.error(error);
+    res.status(500).json({ message: 'Failed to get restaurant details' });
   }
 });
+
 
 app.get('/nearby-restaurants', async (req, res) => {
   const geolocationurl = `https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.GOOGLE_API_KEY}`;
@@ -113,18 +64,12 @@ app.get('/nearby-restaurants', async (req, res) => {
     const geoResponse = await axios.post(geolocationurl, {});
     const { lat, lng } = geoResponse.data.location;
 
-
-
-    // Extract query parameters from the request
     const { keyword = 'restaurant', location = `${lat},${lng}`, radius = 1500, type = 'restaurant' } = req.query;
 
-    // Construct the URL dynamically using the parameters
     const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=${encodeURIComponent(keyword)}&location=${encodeURIComponent(location)}&radius=${radius}&type=${type}&key=${process.env.GOOGLE_API_KEY}`;
 
-    // Make the HTTP request to the Google Places API
     const response = await axios.get(url);
 
-    // Send the API response data back to the client
     res.json(response.data);
   } catch (error) {
     console.error('Error fetching nearby restaurants:', error.message);
